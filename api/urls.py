d0caa2b8c44a9205f688f788b153379877269c80
rolandoandre123 <rbardalespriale@gmail.com>
2024-12-14 18:07:55 -0500

    add : creando app pedro tech api

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blogposts', views.BlogPostListCreate.as_view(),
-         name="blogpost-view-create")
+    path('blogposts', views.BlogPostListCreateView.as_view(),
+         name="blogpost-view-create"),
+    path("blogposts/<int:pk>",
+         views.BlogPostRetrieveUpdateDestroyView.as_view(), name="blogpost-view-update"),
+    path("blogposts-custom", views.BlogPostList.as_view(),
+         name="blogpost-view-list-custom")
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
+from turtle import update
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status, views
+from rest_framework.response import Response
 from .models import BlogPost
 from .serializers import BlogPostSerializer
 
@@ -7,7 +9,30 @@ from .serializers import BlogPostSerializer
 # to handle typical CRUD operations for an API
 
 
-class BlogPostListCreate(generics.ListCreateAPIView):
+class BlogPostListCreateView(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = BlogPostSerializer
+
+    def delete(self, request, *args, **kwargs):
+        BlogPost.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class BlogPostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = "pk"
+
+
+class BlogPostList(views.APIView):
+    def get(self, request, format=None):
+        title = request.query_params.get("title", "")
+
+        if title:
+            blog_posts = BlogPost.objects.filter(title__icontains=title)
+        else:
+            blog_posts = BlogPost.objects.all()
+
+        serializer = BlogPostSerializer(blog_posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
